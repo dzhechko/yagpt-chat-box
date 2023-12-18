f399be7671ad1dbbccdaa79369a63210a2098407
Dmitry Zhechkov <dzhechkov@yandex-team.ru>
2023-12-18 15:46:19 +0300

    first commit

--- YaGPT-Chat.py
+++ YaGPT-Chat.py
@@ -49,9 +49,9 @@ def main():
 
     # использовать системные переменные из облака streamlit (secrets)
     
-    folder_id = st.secrets["yagpt_folder_id"]
-    api_id = st.secrets["yagpt_api_id"]
-    api_key = st.secrets["yagpt_api_key"]
+    yagpt_folder_id = st.secrets["yagpt_folder_id"]
+    yagpt_api_id = st.secrets["yagpt_api_id"]
+    yagpt_api_key = st.secrets["yagpt_api_key"]
 
     # yagpt_folder_id = st.sidebar.text_input("YAGPT_FOLDER_ID", type='password', value=folder_id)
     # yagpt_api_id = st.sidebar.text_input("YAGPT_API_ID", type='password', value=api_id)
